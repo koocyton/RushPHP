8b00c22c24a9c09b7dd8ccf7b3a548a00c1d9668
koocyton <koocyton@gmail.com>
2012-10-22 16:11:01 +0800

    Change-Id: Ica0c25cd9e5b2b193381b437a8787f8167199bab

--- libs/RushPHP/Singleton.php
+++ libs/RushPHP/Singleton.php
@@ -1,23 +1,10 @@
 <?php	  
 namespace RushPHP;
 
-/**
- * 单例管理器
- * 
- * @package RaftPHP
- */
 class Singleton 
 {
     private static $instances = array();
-    
-    /**
-     * 根据类名获取该类的单例
-     * 
-     * @param string $class_name
-     * @param string $unique_key
-	 *
-     * @return Object
-     */ 
+
     public static function get($class_name, $unique_key=null)
     {
 		$unique_key = empty($unique_key) ? $class_name : $unique_key;
